53442d07f8d943a856236e1aaf67bae508381c9f
Yosef Ruvinov <105154265+yosef-ruvinov@users.noreply.github.com>
2024-07-06 20:29:52 +0300

    Update to main.tf
    Adding changes to the main.tf file

--- main.tf
+++ main.tf
@@ -17,21 +17,27 @@ provider "aws" {
 resource "aws_security_group" "tf_sg" {
   name        = "security group using Terraform"
   description = "security group using Terraform"
-  vpc_id      = "vpc-084e88412d2a7d37e"
+  vpc_id      = "vpc-0a534c6a223c8fb8f"
 
   ingress {
-    description      = "SSH"
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }  
+  }
 
   ingress {
-    description      = "SSH"
     from_port        = 22
     to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
@@ -52,10 +58,9 @@ resource "aws_security_group" "tf_sg" {
 
 # Jenkins instance - Master with installed: Ubuntu 22.04, Jenkins, Java, GIT
 resource "aws_instance" "jenkins" {
-  ami             = "ami-0de6215d9c2342df5" # Ubuntu 22.04 LTS AMI ID
-  instance_type   = "t3.micro"
-  key_name        = "terraform-keypair"
-  security_groups = ["aws_security_group.tf_sg"]
+  ami           = "ami-0de6215d9c2342df5" # Ubuntu 22.04 LTS AMI ID
+  instance_type = "t3.micro"
+  key_name      = "terraform_keyPair"
 
   tags = {
     Name = "Jenkins"
@@ -76,10 +81,9 @@ resource "aws_instance" "jenkins" {
 
 # My Ubuntu instance - SLave with installed: Ubuntu 22.04, Java, Docker
 resource "aws_instance" "my_ubuntu" {
-  ami             = "ami-0de6215d9c2342df5" # Ubuntu 22.04 LTS AMI ID
-  instance_type   = "t3.micro"
-  key_name        = "terraform-keypair"
-  security_groups = ["aws_security_group.tf_sg"]
+  ami           = "ami-0de6215d9c2342df5" # Ubuntu 22.04 LTS AMI ID
+  instance_type = "t3.micro"
+  key_name      = "terraform_keyPair"
 
   tags = {
     Name = "My Ubuntu"
@@ -96,10 +100,9 @@ resource "aws_instance" "my_ubuntu" {
 
 # My Windows instance - Slave with installed: Win10, Docker, Java
 resource "aws_instance" "my_windows" {
-  ami             = "ami-07df29cf3e326c3ad" # Windows 10 AMI ID
-  instance_type   = "t3.micro"
-  key_name        = "terraform-keypair"
-  security_groups = ["aws_security_group.tf_sg"]
+  ami           = "ami-07df29cf3e326c3ad" # Windows 10 AMI ID
+  instance_type = "t3.micro"
+  key_name      = "terraform_keyPair"
 
   tags = {
     Name = "My Windows"
