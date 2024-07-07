b06b8f020134d7e377136fbc90b5dca1077f36da
Yosef Ruvinov <105154265+yosef-ruvinov@users.noreply.github.com>
2024-07-07 10:46:52 +0300

    Revert "Merge branch 'main' of https://github.com/yosef-ruvinov/ecommerce-django-react"
    This reverts commit b3bdb60d4580acac9eff2f9a069da6843e2aee15, reversing
    changes made to 05de38ae8f88242fc1bdb0a4c664440f3476e804.

--- main.tf
+++ main.tf
@@ -1,1 +1,128 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.27"
+    }
+  }
+}
 
+# The cloud provider
+provider "aws" {
+  region  = "il-central-1"
+  profile = "terraform-user"
+}
+
+# Security group for SSH access
+resource "aws_security_group" "ec2_sg" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ec2_sg"
+  }
+}
+
+
+# Jenkins instance - Master with installed: Ubuntu 22.04, Jenkins, Java, GIT
+resource "aws_instance" "jenkins" {
+  ami           = "ami-0de6215d9c2342df5" # Ubuntu 22.04 LTS AMI ID
+  instance_type = "t3.micro"
+  key_name      = "terraform_keyPair"
+  security_groups = [aws_security_group.ec2_sg.name]
+
+
+  tags = {
+    Name = "Jenkins"
+  }
+
+ user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y openjdk-17-jdk
+              sudo apt-get install -y git
+              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+              sudo apt-get update
+              sudo apt-get install -y jenkins
+              sudo systemctl start jenkins
+              sudo systemctl enable jenkins
+              EOF
+}
+
+# My Ubuntu instance - SLave with installed: Ubuntu 22.04, Java, Docker
+resource "aws_instance" "my_ubuntu" {
+  ami           = "ami-0de6215d9c2342df5" # Ubuntu 22.04 LTS AMI ID
+  instance_type = "t3.micro"
+  key_name      = "terraform_keyPair"
+  security_groups = [aws_security_group.ec2_sg.name]
+
+  tags = {
+    Name = "My Ubuntu"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y openjdk-17-jdk
+              sudo apt install -y git
+              sudo apt-get install -y docker.io
+              sudo usermod -aG docker ubuntu
+              EOF
+}
+
+# My Windows instance - Slave with installed: Win10, Docker, Java
+resource "aws_instance" "my_windows" {
+  ami           = "ami-07df29cf3e326c3ad" # Windows 10 AMI ID
+  instance_type = "t3.micro"
+  key_name      = "terraform_keyPair"
+  security_groups = [aws_security_group.ec2_sg.name]
+
+  tags = {
+    Name = "My Windows"
+  }
+
+  user_data = <<-EOF
+              <powershell>
+              Install-WindowsFeature -Name Containers
+              Invoke-WebRequest -Uri https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile DockerDesktopInstaller.exe
+              Start-Process -FilePath DockerDesktopInstaller.exe -ArgumentList "/quiet" -Wait
+              Invoke-WebRequest -Uri https://download.java.net/java/GA/jdk17/GPL/openjdk-17_windows-x64_bin.zip -OutFile openjdk-17_windows-x64_bin.zip
+              Expand-Archive openjdk-17_windows-x64_bin.zip -DestinationPath C:\\Java
+              setx /M JAVA_HOME C:\\Java\\jdk-17
+              setx /M PATH "%PATH%;C:\\Java\\jdk-17\\bin"
+              </powershell>
+              EOF
+  }
