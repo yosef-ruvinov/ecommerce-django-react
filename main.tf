ecef2c03ca0a3fc91b4b4c2ac33d110fc6669b4b
yosef-ruvinov <yossiruvinov@gmail.com>
2024-07-09 02:24:29 +0300

    maintf update

--- main.tf
+++ main.tf
@@ -71,6 +71,8 @@ resource "aws_instance" "jenkins" {
                 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
                 sudo apt-get update
                 sudo apt-get install -y jenkins
+                sudo apt-get install -y docker.io
+                sudo usermod -aG docker jenkins
                 sudo systemctl start jenkins
                 sudo systemctl enable jenkins
                 sleep 30
