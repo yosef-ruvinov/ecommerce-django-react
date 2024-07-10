ab2d0a3d73b1fe13f60e5ad90d9d57b0e1a84324
yosef-ruvinov <yossiruvinov@gmail.com>
2024-07-11 02:22:22 +0300

    minor chg

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# variable "rds_endpoint" {
+#   type        = "my_rds"
+#   description = "The endpoint (hostname) of the RDS instance"
+# }
+
+# variable "mysql_user" {
+#   type        = string
+#   description = "MySQL username for connecting to the RDS instance"
+# }
+
+# variable "mysql_password" {
+#   type        = string
+#   description = "MySQL password for connecting to the RDS instance"
+# }
