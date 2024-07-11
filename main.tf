2ddaed9187cecc4bd94d6c4c85c08ad662c25e9d
yosef-ruvinov <yossiruvinov@gmail.com>
2024-07-11 20:49:26 +0300

    CRIT upt

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "ec2_sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
+  } 
 
   ingress {
     from_port        = 3306
