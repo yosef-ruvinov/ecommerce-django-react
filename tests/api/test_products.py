bdfaeb00730baaeb18bb18ea38e5e8360b69e9d5
yosef-ruvinov <yossiruvinov@gmail.com>
2024-07-16 23:33:20 +0300

    upt

--- tests/api/test_products.py
+++ tests/api/test_products.py
@@ -1,39 +1,34 @@
 import pytest
-
-# @pytest.mark.django_db
-# def test_product_created():
-#   Product.objects.create
-from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
-
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 from base.models import Product
 
-
 def create_product():
-  return Product.objects.create(
-        name=" Product Name ",
+    return Product.objects.create(
+        name="Product Name",
         price=0,
-        brand="Sample brand ",
+        brand="Sample brand",
         countInStock=0,
         category="Sample category",
-        description=" ")
+        description="Sample description"
+    )
 
 @pytest.mark.django_db
 def test_product_creation():
-  p = create_product()
-  assert isinstance(p, Product) is True
-  assert p.name == " Product Name "
+    p = create_product()
+    assert isinstance(p, Product) is True
+    assert p.name == "Product Name"
 
-
-
-
-
-# Api test  - Integration testing
+@pytest.mark.django_db
 def test_api_product_creation():
+    # Create a user and obtain a token
+    user = User.objects.create_user(username='testuser', password='testpassword')
+    token = Token.objects.create(user=user)
+
     client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
     response = client.post("/api/products/create/")
-
-    # data = response.data
 
     assert response.status_code == 200
