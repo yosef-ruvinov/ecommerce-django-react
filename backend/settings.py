653a54b2f759e9628966e4734d8f7c221bc89647
yosef-ruvinov <yossiruvinov@gmail.com>
2024-07-18 00:04:26 +0300

    upt

--- backend/settings.py
+++ backend/settings.py
@@ -128,17 +128,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my_sql_database',
-        'USER': 'ecommerce_db',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
