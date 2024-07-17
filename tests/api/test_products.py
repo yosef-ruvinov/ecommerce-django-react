31b1068d7ca47e74d3845e531ceb63ea05be7e9d
yosef-ruvinov <yossiruvinov@gmail.com>
2024-07-17 10:18:04 +0300

    upt

--- tests/api/test_products.py
+++ tests/api/test_products.py
@@ -1,31 +1,26 @@
 import pytest
-
-# @pytest.mark.django_db
-# def test_product_created():
-#   Product.objects.create
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import AccessToken
 from django.contrib.auth.models import User
 from base.models import Product
-from rest_framework_simplejwt.tokens import AccessToken  # Adjust based on your JWT library
-
 
 def create_product():
-  return Product.objects.create(
-        name=" Product Name ",
+    return Product.objects.create(
+        name="Product Name",
         price=0,
-        brand="Sample brand ",
+        brand="Sample brand",
         countInStock=0,
         category="Sample category",
-        description=" ")
+        description="Sample description"
+    )
 
 @pytest.mark.django_db
-
 def get_access_token():
-    # Implement a function to retrieve a valid JWT access token
-    # This could involve logging in or fetching from a mock token generator
-    # Example:
-    user = User.objects.get(username='test_user')
+    user, created = User.objects.get_or_create(username='test_user')
+    if created:
+        user.set_password('test_password')
+        user.save()
     access_token = AccessToken.for_user(user)
     return str(access_token)
 
